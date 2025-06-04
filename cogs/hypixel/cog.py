d6cf62b30758348ce056fd82a80b93ec53aef518
Blue-Robin-Taken <julianjohnt@gmail.com>
2025-06-04 17:03:42 -0500

    FIX: UUID bug with numbers

--- cogs/hypixel/cog.py
+++ cogs/hypixel/cog.py
@@ -12,7 +12,7 @@ class HypixelCog(commands.Cog):
 
     @staticmethod
     async def sanitize_characters(inputString):
-        checkChars = string.ascii_letters + '_-'
+        checkChars = string.ascii_letters + '_-' + '1234567890'
         finalString = ''
         for char in inputString:
             if char in checkChars:
@@ -37,6 +37,7 @@ class HypixelCog(commands.Cog):
     @discord.slash_command(guilds_ids=[os.environ['GUILD_ID']])
     async def hypixel_stats(self, context: discord.ApplicationContext, user: str):
         mojangUserUUID = await self.sanitize_characters(user)
+        username = ''
         if not len(user) > 16:
             # fetch to get the user's UUID
             mojangRequest = requests.get(
@@ -47,11 +48,30 @@ class HypixelCog(commands.Cog):
                 return await context.respond('User does not exist', ephemeral=True)
 
             mojangUserUUID = mojangRequestJSON['id']
-        # Make the request to Hypixel
-        hypixelRequest = requests.get(f"https://api.hypixel.net/v2/player?uuid={mojangUserUUID}", headers={'API-KEY': os.environ['HYPIXEL_API_KEY']})
-        hypixelRequestJSON = json.loads(hypixelRequest.text)
+            username = mojangRequestJSON["name"]
+        else:  # Get username through provided UUID
+            print(await self.sanitize_characters(user.strip()))
+            mojangRequest = requests.get(
 
-        await context.respond(mojangUserUUID)
+                f"https://api.minecraftservices.com/minecraft/profile/lookup/{await self.sanitize_characters(user.strip())}")
+            mojangRequestJSON = json.loads(mojangRequest.text)
+            print(mojangRequestJSON)
+            username = mojangRequestJSON['name']
+        # Make the request to Hypixel
+        hypixelRequestPlayer = requests.get(f"https://api.hypixel.net/v2/player?uuid={mojangUserUUID}",
+                                            headers={'API-KEY': os.environ['HYPIXEL_API_KEY']})
+        hypixelRequestPlayerJSON = json.loads(hypixelRequestPlayer.text)
+
+        hypixelRequestGuild = requests.get(f'https://api.hypixel.net/v2/guild?player={mojangUserUUID}',
+                                           headers={'API-KEY': os.environ['HYPIXEL_API_KEY']})
+        hypixelRequestGuildJSON = json.loads(hypixelRequestGuild.text)
+        print(hypixelRequestPlayerJSON)
+
+        guildName = hypixelRequestGuildJSON['guild']['name']
+
+        finalEmbed = discord.Embed(title=f'Stats for: {username}',
+                                   description=f"Guild: {guildName}")
+        await context.respond(embed=finalEmbed)
 
 
 def setup(bot):
