54791dca381a7e0a52f15df0f78b52b35b654d4a
Blue-Robin-Taken <julianjohnt@gmail.com>
2025-06-04 16:22:14 -0500

    added get uuid command

--- cogs/hypixel/cog.py
+++ cogs/hypixel/cog.py
@@ -5,25 +5,48 @@ import requests
 import json
 import string
 
+
 class HypixelCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @staticmethod
     async def sanitize_characters(inputString):
-        checkChars = string.ascii_letters + '_'
+        checkChars = string.ascii_letters + '_-'
         finalString = ''
         for char in inputString:
             if char in checkChars:
                 finalString += char
         return finalString
 
+    @discord.slash_command(guild_ids=[os.environ['GUILD_ID']])
+    async def get_minecraft_uuid(self, context: discord.ApplicationContext, user: str):
+        mojangRequest = requests.get(
+            f"https://api.mojang.com/users/profiles/minecraft/{await self.sanitize_characters(user.strip())}")
+        mojangRequestJSON = json.loads(mojangRequest.text)
+        if 'id' not in mojangRequestJSON.keys():
+            return await context.respond('User does not exist', ephemeral=True)
+        mojangUserUUID = mojangRequestJSON['id']
+        embed = discord.Embed(
+            title=f'User: {mojangRequestJSON["name"]}',
+            description=f"UUID: {mojangUserUUID}",
+            color=discord.Color.random()
+        )
+        await context.respond(embed=embed)
+
     @discord.slash_command(guilds_ids=[os.environ['GUILD_ID']])
     async def hypixel_stats(self, context: discord.ApplicationContext, user: str):
-        mojangUserUUID = ""
+        mojangUserUUID = user
         if not len(user) > 16:
             # fetch to get the user's
-            mojangUserUUID = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{await self.sanitize_characters(user.strip())}")
+            mojangRequest = requests.get(
+                f"https://api.mojang.com/users/profiles/minecraft/{await self.sanitize_characters(user.strip())}")
+            mojangRequestJSON = json.loads(mojangRequest.text)
+
+            if 'id' not in mojangRequestJSON.keys():
+                return await context.respond('User does not exist', ephemeral=True)
+
+            mojangUserUUID = mojangRequest['id']
         await context.respond(mojangUserUUID.content)
 
 
